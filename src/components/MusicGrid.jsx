7bc87453aa014d2896a15ca316f1c4bce25092ff
Alec Johnson <alecjohnson55@gmail.com>
2020-03-28 10:37:02 -0700

    make pink cells cycle through

--- src/components/MusicGrid.jsx
+++ src/components/MusicGrid.jsx
@@ -3,21 +3,39 @@ import GridColumn from './GridColumn';
 import GridElement from './GridElement';
 
 class MusicGrid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playingCol: 0,
+        };
+        this.gridSize = 10;
+    }
+
+    componentDidMount() {
+        setInterval(this.cyclePlaying, 200)
+    }
+
+    cyclePlaying = (prevState, props) => {
+        this.setState((prevState, props) => ({
+            playingCol: (prevState.playingCol + 1) % this.gridSize
+        }))
+    }
+
     createGrid() {
         let cols = []
         for (let i = 0; i < 10; i++) {
             let row = []
             for (let j = 0; j < 10; j++) {
+                const name = `${i}-${j}`
                 row.push(
                     <GridElement
-                        selected={(i === 0 || i === 3) && j === 5}
-                        key={`${i}-${j}`}
+                        key={name}
                     ></GridElement>
                 )
             }
             cols.push(
                 <GridColumn
-                    playing={i === 0}
+                    playing={i === this.state.playingCol}
                     key={`${i}`}
                     row={row}
                 >
