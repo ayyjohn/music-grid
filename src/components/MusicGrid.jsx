bbb53183621e2251dcc89f4167f0665c678deb0d
Alec Johnson <alecjohnson55@gmail.com>
2020-03-27 21:08:13 -0700

    switch rows and cols

--- src/components/MusicGrid.jsx
+++ src/components/MusicGrid.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 class MusicGrid extends React.Component {
     createGrid() {
-        let rows = []
+        let cols = []
         for (let i = 0; i < 10; i++) {
-            let cols = []
+            let row = []
             for (let j = 0; j < 10; j++) {
-                cols.push(<td className="grid-el" key={`${i-j}`}></td>)
+                row.push(<td className="grid-el" key={`${i-j}`}></td>)
             }
             if (i === 0) {
-                rows.push(<tr className="grid-row-playing" key={i}>{cols}</tr>)
+                cols.push(<tr className="grid-row-playing" key={i}>{cols}</tr>)
             } else {
-                rows.push(<tr className="grid-row" key={i}>{cols}</tr>)
+                cols.push(<tr className="grid-row" key={i}>{cols}</tr>)
             }
         }
-        return rows
+        return cols
     }
 
     render() {
